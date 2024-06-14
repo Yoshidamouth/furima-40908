d1c256cc287d65fc1400d85ec809a13c22f75834
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-14 10:58:06 +0900

    モデル単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 ,message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 ,message: "can't be blank"} 
   validates :shipping_day_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :image, presence: true
 
   has_many :comments
   belongs_to :user
