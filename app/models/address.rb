26c87ea7db02116ee1cc96a5d606a1665a4d141c
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-18 22:19:21 +0900

    バリテーション/アソシエーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,14 @@
 class Address < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  validates :postcode, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+  validates :prefecture_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :municipalities, presence: true
+  validates :street_address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input half-width characters."}
+
   belongs_to :order
 
 end
