6f24cbe9f2cf9f1dd33f8c4905cf3ce8d460e65e
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-21 17:18:00 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_address =  OrderAddress.new
   end
@@ -13,9 +14,11 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address =  OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +26,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postcode, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -9,6 +9,7 @@ class OrderAddress
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input half-width characters."}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 ,message: "can't be blank"} 
 
