9a0b1e9b9f5155d2d53a6922ca9b23ecd362e753
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-22 16:58:08 +0900

    テストコード実装

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    token {"tok_abcdefghijk00000000000000000"}
+    postcode {"123-1111"}
+    prefecture_id { 2 }
+    municipalities {"横浜"} 
+    street_address {"横浜1-1-1"}
+    building_name {"横ビル"}
+    phone_number {"08000000000"}
+    item_id { create(:item).id }
+    user_id { create(:user).id }
+  end
+end
